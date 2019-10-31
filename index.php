ab6df2f873a34820b47d657f141da5433e86fa93
Omilia25 <omiliakelvin@gmail.com>
2019-10-31 05:07:20 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Royal Fortune</title>
+	<link rel="stylesheet" type="text/css" href="index.css">
+	<!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
+</head>
+<body class="txt-center">
+	<div class="parallax">
+		<div class="heading">
+			<h1>
+				ROYAL FORTUNE & J SOLUTIONS LIMITED
+			</h1>
+		</div>
+		<div class="content">Royal fortunes is a leading real estate egency in Kenya. We deal in real estate, plots and land. We manage prime real estate in Kenya and now we introduce Golden Century Apartments in Kileleshwa.<br>0711777446, 0723074552, 0785325728<br>royalfandjsolutionsltd@gmail.com</div>
+		<a href="#" class="btn">Read More</a>
+	</div>
+
+	<div class="text" id="grey">
+		<h1>GOLDEN CENTURY APARTMENTS</h1>
+		<h3>A building that dominates Nairobi's sky line.</h3>
+		<div class="two-col">
+			<p>For the executive on the move, life revolves around time and accessibility and Golden Century Apartments is in its strategic location in Lavington area along Othaya road, bordering Kileleshwa which leads to all the corporate venues and business locations that you need to get to quickly.With only two blocks of three and four  bedroom apartments each unit remains structurally exclusive and alive with free-flow of natural light and fresh air circulation. A long, common passage area between the two blocks ensures total privacy.</p>
+		</div>
+		<div class="two-col">
+			<p>Golden Century apartments comprise of 3 & 4 bed all en-suite apartments with DSQ, located strategically along Othaya road, in Kileleshwa. With only two blocks, each unit remains structurally exclusive with free flow of natural light and air. The development has 108 units (60 3 Beds & 48 4 Beds) on 12 levels and one acre piece of land. Show house ready.</p>
+		</div>
+	</div>
+
+	<div class="parallax2">
+		<div class="heading-sm">
+			GOLDEN CENTURY APARTMENTS
+		</div>
+	</div>
+
+	<div class="text" id="dark">
+		<h2>Quality Finishes</h2>
+		<p>Features: High quality wooden flooring in the sitting areas, corridors and bedrooms, European premium ceramic tiles in the kitchen, balconies and bathrooms, European high quality sanitary ware & fittings, reserved connection points for TV & internet, pantry and utility area, spacious living room, guest cloak room, both solar water heating & electric heating option, borehole, back-up generator, 2 high speed lifts on each block and spacious DSQ with independent access from the main lobby. Show house,ready. Anticipated completion: November 2019.</p>
+	</div>
+
+	<div class="parallax3">		
+	</div>
+
+	<div class="text" id="dark">
+		<h3>Great Amenities and Pricing</h3>
+		<div class="two-col">
+			<p>Amenities: Fully equipped gym, swimming pool, landscaped gardens, round the clock security and a dedicated guard room at the entrance, intercom system for each apartment, adequate parking spaces etc. </p>
+		</div>
+
+		<div class="two-col">
+			<p><strong>Pricing</strong><br>
+				3 Bed all en-suite plus SQ (196)/(212 sq.m) – From Kes. 20M<br>
+				4 Bed all en-suite plus SQ (246 sq.m) – Kes. 25M
+			</p>
+		</div>
+		<div><br>0711777446, 0723074552, 0785325728<br>royalfandjsolutionsltd@gmail.com</div>
+	</div>
+
+	<div class="parallax4">
+	</div>
+	<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
